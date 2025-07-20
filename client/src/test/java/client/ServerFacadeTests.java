b72c08e19a4862da599fb1870f18d5fb8436ffef
jackmanj1337 <jackmanj1337@gmail.com>
2025-07-19 18:32:08 -0600

    added all server facade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -68,29 +68,72 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void RegisterTest() {
+    public void registerTest() {
         RegisterResult result = facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
         assertEquals(200, result.httpCode());
     }
 
     @Test
-    public void RegisterTestNullPassword() {
+    public void registerTestNullPassword() {
         RegisterResult result = facade.register(new RegisterRequest("doug", null, "doug@testing.com"));
         assertEquals(400, result.httpCode());
     }
 
     @Test
-    public void LoginTest() {
+    public void loginTest() {
         facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
         LoginResult result = facade.login(new LoginRequest("doug", "password"));
         assertEquals(200, result.httpCode());
     }
 
     @Test
-    public void LoginBadPasswordTest() {
+    public void loginBadPasswordTest() {
         facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
         LoginResult result = facade.login(new LoginRequest("doug", "passeord"));
         assertEquals(401, result.httpCode());
+    }
+
+    @Test
+    public void createGameTest() {
+        RegisterResult rResult = facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
+        CreateGameResult cGResult = facade.createGame(new CreateGameRequest(rResult.authToken(), "coolGame"));
+        assertEquals(200, cGResult.httpCode());
+    }
+
+    @Test
+    public void createGameBadAuthTest() {
+        CreateGameResult cGResult = facade.createGame(new CreateGameRequest("spoofedToken", "coolGame"));
+        assertEquals(401, cGResult.httpCode());
+    }
+
+    @Test
+    public void listGamesTest() {
+        RegisterResult rResult = facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
+        ListGamesResult lGResult = facade.listGames(new ListGamesRequest(rResult.authToken()));
+        assertEquals(200, lGResult.httpCode());
+    }
+
+    @Test
+    public void listGamesBadAuthTest() {
+        ListGamesResult lGResult = facade.listGames(new ListGamesRequest("badToken"));
+        assertEquals(401, lGResult.httpCode());
+    }
+
+    @Test
+    public void joinGameTest() {
+        RegisterResult rResult = facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
+        CreateGameResult cGResult = facade.createGame(new CreateGameRequest(rResult.authToken(), "coolGame"));
+        JoinGameResult jGResult = facade.joinGame(new JoinGameRequest(rResult.authToken(), "WHITE", cGResult.gameID()));
+        assertEquals(200, jGResult.httpCode());
+    }
+
+    @Test
+    public void joinGameColorTakenTest() {
+        RegisterResult rResult = facade.register(new RegisterRequest("doug", "password", "doug@testing.com"));
+        CreateGameResult cGResult = facade.createGame(new CreateGameRequest(rResult.authToken(), "coolGame"));
+        facade.joinGame(new JoinGameRequest(rResult.authToken(), "WHITE", cGResult.gameID()));
+        JoinGameResult jGResult = facade.joinGame(new JoinGameRequest(rResult.authToken(), "WHITE", cGResult.gameID()));
+        assertEquals(403, jGResult.httpCode());
     }
 
 
