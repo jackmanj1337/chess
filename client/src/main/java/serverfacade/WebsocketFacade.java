24b124c3c56007344ef81b24e9566573f7f9a34d
jackmanj1337 <jackmanj1337@gmail.com>
2025-07-24 16:56:58 -0600

    switched client from jetty to javax

--- client/src/main/java/serverfacade/WebsocketFacade.java
+++ client/src/main/java/serverfacade/WebsocketFacade.java
@@ -1,17 +1,26 @@
 package serverfacade;
 
 import com.google.gson.Gson;
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.annotations.*;
-import org.eclipse.jetty.websocket.client.WebSocketClient;
 import postlogin.gameplay.NotificationManager;
 import utilities.ServerConnectionSettings;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import java.io.IOException;
 import java.net.URI;
 
-@WebSocket
+
+@ClientEndpoint
 public class WebsocketFacade {
 
     private String authToken;
@@ -25,62 +34,50 @@ public class WebsocketFacade {
         establishWebsocketConnection();
     }
 
-    public void establishWebsocketConnection() {
-
-        WebSocketClient client = new WebSocketClient();
-        WebsocketFacade socket = this;
-
+    private void establishWebsocketConnection() {
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         try {
-            client.start();
             URI uri = new URI("ws://" + ServerConnectionSettings.SERVER_LOCATION + "/ws");
-            client.connect(socket, uri).get();
-
-        } catch (Throwable t) {
-            t.printStackTrace();
+            container.connectToServer(this, uri);
+        } catch (DeploymentException | IOException | java.net.URISyntaxException e) {
+            e.printStackTrace();
         }
     }
 
-    @OnWebSocketConnect
-    public void onConnect(Session session) {
+    @OnOpen
+    public void onOpen(Session session) {
         this.session = session;
         System.out.println("Connected to server");
 
         UserGameCommand connect = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
 
         try {
-            session.getRemote().sendString(GSON.toJson(connect));
-        } catch (Exception e) {
+            session.getBasicRemote().sendText(GSON.toJson(connect));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    @OnWebSocketMessage
+    @OnMessage
     public void onMessage(String msg) {
         System.out.println("Received message from server: " + msg);
         ServerMessage message = GSON.fromJson(msg, ServerMessage.class);
         switch (message.getServerMessageType()) {
-            case NOTIFICATION:
-                NotificationManager.handleNotification(message.getMessage());
-                break;
-            case ERROR:
-                NotificationManager.handleError(message.getErrorMessage());
-                break;
-            case LOAD_GAME:
-                NotificationManager.handleLoadGame(message.getGame());
-                break;
-            default:
-                System.out.println("Unhandled message type: " + message.getServerMessageType());
-                break;
+            case NOTIFICATION -> NotificationManager.handleNotification(message.getMessage());
+            case ERROR -> NotificationManager.handleError(message.getErrorMessage());
+            case LOAD_GAME -> NotificationManager.handleLoadGame(message.getGame());
+            default -> System.out.println("Unhandled message type: " + message.getServerMessageType());
         }
     }
 
-    @OnWebSocketClose
-    public void onClose(int statusCode, String reason) {
-        System.out.println("Connection closed: " + reason);
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        System.out.println("Connection closed: " + reason.getReasonPhrase());
+        this.session = null;
     }
 
-    @OnWebSocketError
-    public void onError(Throwable cause) {
+    @OnError
+    public void onError(Session session, Throwable cause) {
         cause.printStackTrace();
     }
 
@@ -88,12 +85,11 @@ public class WebsocketFacade {
         String message = GSON.toJson(command);
         if (session != null && session.isOpen()) {
             try {
-                session.getRemote().sendString(message);
-            } catch (Exception e) {
+                session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
-
 }
+
